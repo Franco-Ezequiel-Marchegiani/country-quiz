4b96f7371537d564f62722436bcc4ed636d93252
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-10 20:32:47 -0300

    Save a API of flags

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     /* Puedo hacer que sean con DOS llamados a distintas APIs
     Que una API sea de paises
-    Y otra de capitales de ciudades */
+    Y otra de capitales de ciudades
+    API Flags: https://www.countryflagsapi.com/ */
     <main className='containerAll'>
       <div>
       <h1>Country Quiz</h1>
