7d895d7e2b477b281b88548ed616870f2ddb1856
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-10 21:40:43 -0300

    Put the image on the view

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,7 +5,7 @@ function Question() {
   return (
       <section>
         <h2>Kuala Lumpur is the capital of</h2>
-        
+        <img src={LogoWorld} alt="" />
         <button>A Texto 1</button>
         <button>B Texto 2</button>
         <button>C Texto 3</button>
