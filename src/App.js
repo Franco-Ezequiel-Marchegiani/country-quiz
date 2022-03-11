4598ac2a8517da0e5875814e203019b71f8bc9f5
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-10 21:35:25 -0300

    Save the apis which i'll work tomorrow

--- src/App.js
+++ src/App.js
@@ -6,7 +6,22 @@ function App() {
     /* Puedo hacer que sean con DOS llamados a distintas APIs
     Que una API sea de paises
     Y otra de capitales de ciudades
-    API Flags: https://www.countryflagsapi.com/ */
+    API Flags: https://www.countryflagsapi.com/ 
+    API Countries/capitales 
+    var myHeaders = new Headers();
+myHeaders.append("apikey", "Q1dDLxCfahEE8WEQFGghtx7OQtTEWmUo");
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/geo/city/name/{city}", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+  */
     <main className='containerAll'>
       <div>
       <h1>Country Quiz</h1>
