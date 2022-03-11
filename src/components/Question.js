a53b3c91493347ca12da9db45928cd7aedf3784f
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-10 21:40:31 -0300

    Import the image

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import LogoWorld from '../images/undraw_winners_ao2o 2.svg'
 function Question() {
   return (
       <section>
         <h2>Kuala Lumpur is the capital of</h2>
+        
         <button>A Texto 1</button>
         <button>B Texto 2</button>
         <button>C Texto 3</button>
