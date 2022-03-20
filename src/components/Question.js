2001031e23bcb39f2505b2ea9767355ed8e8f2fa
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-19 23:59:40 -0300

    Change the question of the flagquestion N°6

--- src/components/Question.js
+++ src/components/Question.js
@@ -63,7 +63,7 @@ function Question() {
       ]
     },
     {
-      questionText: "De qué color es el caballo blanco de San Martín?",
+      questionText: "A qué país corresponde esta bandera?",
       flagCode: "nzl",
       answerOptiones: [
         { answerText: "Azul", isCorrect: false},
