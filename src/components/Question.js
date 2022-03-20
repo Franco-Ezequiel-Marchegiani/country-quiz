c4a62a9c4aec963b3ade746c1e22ead49bc0906f
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-20 01:10:46 -0300

    Add the question in English in the question N°2

--- src/components/Question.js
+++ src/components/Question.js
@@ -25,6 +25,7 @@ function Question() {
     },
     {
       questionText: "A qué país le pertenece esta bandera?",
+      questionTextEnglish: "Which country does this flag belong to",
       flagCode: "br",
       answerOptiones: [
         { answerText: "Azul", isCorrect: false},
