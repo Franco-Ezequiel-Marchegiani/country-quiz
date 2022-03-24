5c607c298f752501a18c7122b54c1afbc84317cd
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-23 22:48:03 -0300

    Add the questionTextEnglish on the question on countriesQuestions

--- src/components/Question.js
+++ src/components/Question.js
@@ -179,6 +179,7 @@ function Question() {
     },
     {
       questionText: "De qué color es el caballo blanco de San Martín?",
+      questionTextEnglish: "Which country does this flag belong to",
       answerOptiones: [
         { answerText: "Azul", isCorrect: false},
         { answerText: "Verde", isCorrect: false},
