692ae07466154e826da89763d71dff974b1c5eee
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-31 15:36:25 -0300

    Set the route of the countries's question

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import QuestionFlag from './components/QuestionFlag';
+import QuestionCountry from './components/QuestionCountry';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.render(
     <Routes >
       <Route path="/" element={<App />} />
       <Route path="/flagQuiz" element={<QuestionFlag/>} />
+      <Route path="/countriesQuiz" element={<QuestionCountry/>} />
     </Routes>
     </BrowserRouter>
   </React.StrictMode>,
