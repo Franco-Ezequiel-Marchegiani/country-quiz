b812d2011df469b1612ac289db3b8f8dcf63864e
Franco Ezequiel <eze.cavs@hotmail.com>
2022-03-31 15:26:37 -0300

    Created the route of QuestionFlag

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Question from './components/Question';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes >
       <Route path="/" element={<App />} />
+      <Route path="/flagQuiz" element={<Question/>} />
     </Routes>
     </BrowserRouter>
   </React.StrictMode>,
