f9964e4cb4497b1b5f20223ad2b3ab68acaa481e
Franco Ezequiel <eze.cavs@hotmail.com>
2022-04-08 16:17:22 -0300

    Put the correct array in teh Question Country

--- src/components/QuestionCountry.js
+++ src/components/QuestionCountry.js
@@ -15,29 +15,29 @@ function QuestionCountry({language, setLanguage}) {
 
   /* Las variables son para ahorrar código en el return para añadir y quitar las clases para los estilos de hover */
   let btnClassNumber1Btn1 = !onHoverBtn1 ? "btnAnswer" : "btnAnswer hoverSelect"
-  let btnClassNumber2Btn1 = answerSelected !== "" ? flagQuestions[numberQuestion].answerOptiones[0].isCorrect === false ? "btnAnswer wrongAnswer" : flagQuestions[numberQuestion].answerOptiones[0].isCorrect === true ? "btnAnswer correctAnswer" : "" : "btnAnswer"
+  let btnClassNumber2Btn1 = answerSelected !== "" ? countriesQuestions[numberQuestion].answerOptiones[0].isCorrect === false ? "btnAnswer wrongAnswer" : countriesQuestions[numberQuestion].answerOptiones[0].isCorrect === true ? "btnAnswer correctAnswer" : "" : "btnAnswer"
   let bothClassesBtn1 = btnClassNumber1Btn1 + " " + btnClassNumber2Btn1 +" "
 
 
   let btnClassNumber1Btn2 = !onHoverBtn2 ? "btnAnswer" : "btnAnswer hoverSelect"
-  let btnClassNumber2Btn2 = answerSelected !== "" ? flagQuestions[numberQuestion].answerOptiones[1].isCorrect === false ? "btnAnswer wrongAnswer" : flagQuestions[numberQuestion].answerOptiones[1].isCorrect === true ? "btnAnswer correctAnswer" : "" : "btnAnswer"
+  let btnClassNumber2Btn2 = answerSelected !== "" ? countriesQuestions[numberQuestion].answerOptiones[1].isCorrect === false ? "btnAnswer wrongAnswer" : countriesQuestions[numberQuestion].answerOptiones[1].isCorrect === true ? "btnAnswer correctAnswer" : "" : "btnAnswer"
   let bothClassesBtn2 = btnClassNumber1Btn2 + " " + btnClassNumber2Btn2
 
 
   let btnClassNumber1Btn3 = !onHoverBtn3 ? "btnAnswer" : "btnAnswer hoverSelect"
-  let btnClassNumber2Btn3 = answerSelected !== "" ? flagQuestions[numberQuestion].answerOptiones[2].isCorrect === false ? "wrongAnswer" : flagQuestions[numberQuestion].answerOptiones[2].isCorrect === true ? "correctAnswer" : "" : ""
+  let btnClassNumber2Btn3 = answerSelected !== "" ? countriesQuestions[numberQuestion].answerOptiones[2].isCorrect === false ? "wrongAnswer" : countriesQuestions[numberQuestion].answerOptiones[2].isCorrect === true ? "correctAnswer" : "" : ""
   let bothClassesBtn3 = btnClassNumber1Btn3 + " " + btnClassNumber2Btn3
   
 
   let btnClassNumber1Btn4 = !onHoverBtn4 ? "btnAnswer" : "btnAnswer hoverSelect"
-  let btnClassNumber2Btn4 = answerSelected !== "" ? flagQuestions[numberQuestion].answerOptiones[3].isCorrect === false ? "btnAnswer wrongAnswer" : flagQuestions[numberQuestion].answerOptiones[3].isCorrect === true ? "btnAnswer correctAnswer" : "" : "btnAnswer"
+  let btnClassNumber2Btn4 = answerSelected !== "" ? countriesQuestions[numberQuestion].answerOptiones[3].isCorrect === false ? "btnAnswer wrongAnswer" : countriesQuestions[numberQuestion].answerOptiones[3].isCorrect === true ? "btnAnswer correctAnswer" : "" : "btnAnswer"
   let bothClassesBtn4 = btnClassNumber1Btn4 + " " + btnClassNumber2Btn4
 
-  let flagCode = flagQuestions[numberQuestion].flagCode;
+  let flagCode = countriesQuestions[numberQuestion].flagCode;
   let urlImage = `https://countryflagsapi.com/png/${flagCode}`;
 
-  let textSpanishTitle = flagQuestions[numberQuestion].questionText;
-  let textEnglishTitle = flagQuestions[numberQuestion].questionTextEnglish;
+  let textSpanishTitle = countriesQuestions[numberQuestion].questionText;
+  let textEnglishTitle = countriesQuestions[numberQuestion].questionTextEnglish;
   return (
     <main className='containerAll'>
       <h1 className='titleApp'>Country Quiz</h1>
@@ -49,7 +49,7 @@ function QuestionCountry({language, setLanguage}) {
                         <img className='imageFlag' src={urlImage} alt="" />
                         <h2 className='questionText'>{language === true ? textSpanishTitle : textEnglishTitle}</h2>
                         {/* Acá se pasa todo lo necesario para utilizar el componente ButtonComponent */}
-                          <ButtonComponent contadorPuntaje={contadorPuntaje} flagQuestions={flagQuestions} numberQuestion={numberQuestion}setNumberQuestion={setNumberQuestion} setContadorPuntaje={setContadorPuntaje} bothClassesBtn1={bothClassesBtn1} bothClassesBtn2={bothClassesBtn2} bothClassesBtn3={bothClassesBtn3} bothClassesBtn4={bothClassesBtn4} answerSelected={answerSelected} setAnswerSelected={setAnswerSelected} wrongAnswerSelected={wrongAnswerSelected} setWrongAnswerSelected={setWrongAnswerSelected} language={language} setLanguage={setLanguage} />
+                          <ButtonComponent contadorPuntaje={contadorPuntaje} flagQuestions={countriesQuestions} numberQuestion={numberQuestion}setNumberQuestion={setNumberQuestion} setContadorPuntaje={setContadorPuntaje} bothClassesBtn1={bothClassesBtn1} bothClassesBtn2={bothClassesBtn2} bothClassesBtn3={bothClassesBtn3} bothClassesBtn4={bothClassesBtn4} answerSelected={answerSelected} setAnswerSelected={setAnswerSelected} wrongAnswerSelected={wrongAnswerSelected} setWrongAnswerSelected={setWrongAnswerSelected} language={language} setLanguage={setLanguage} />
                   </main> 
                   : 
                   <ResultView contadorPuntaje={contadorPuntaje} setNumberQuestion={setNumberQuestion} setContadorPuntaje={setContadorPuntaje} language={language} setLanguage={setLanguage} />
