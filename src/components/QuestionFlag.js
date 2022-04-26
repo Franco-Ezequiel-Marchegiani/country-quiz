58c5fbf4b632291e6af52345d2c2548b5432d68e
Franco Ezequiel <eze.cavs@hotmail.com>
2022-04-25 22:50:07 -0300

    Fix a bug in the image

--- src/components/QuestionFlag.js
+++ src/components/QuestionFlag.js
@@ -39,6 +39,8 @@ function QuestionFlag({language, setLanguage}) {
 
   let flagCode = flagQuestions[numberQuestion].flagCode;
   let urlImage = `https://countryflagsapi.com/png/${flagCode}`;
+  /* ../images/banderas/${flagCode}.png */
+  let urlImage1 = `../images/background.png`;
 
   let textSpanishTitle = flagQuestions[numberQuestion].questionText;
   let textEnglishTitle = flagQuestions[numberQuestion].questionTextEnglish;
