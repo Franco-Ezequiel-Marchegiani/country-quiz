2d9e0169e7250bb54f8b74d6f601cf79af4bd6d5
Franco Ezequiel <eze.cavs@hotmail.com>
2022-04-25 22:58:23 -0300

    Clean code

--- src/components/functionalButtons/BtnChangeLanguage.js
+++ src/components/functionalButtons/BtnChangeLanguage.js
@@ -4,9 +4,8 @@ import English from '../../images/englishFlag.png';
 /* Queda pasar todo lo que requiere para su funcionamiento */
 function BtnChangeLanguage({language, setLanguage}) {
   return (
-    <button className='btnChangeLanguage' onClick={()=> setLanguage(!language) }>
-        <img className='imageFlagLanguage' src={language === true ? Spanish : English} alt="Change Language" />
-        
+        <button className='btnChangeLanguage' onClick={()=> setLanguage(!language) }>
+            <img className='imageFlagLanguage' src={language === true ? Spanish : English} alt="Change Language" />
         </button>
   );
 }
