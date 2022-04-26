263f76fece01c76a0e753d9c31707111e7000a9a
Franco Ezequiel <eze.cavs@hotmail.com>
2022-04-25 22:57:32 -0300

    Clean code

--- src/components/functionalButtons/BtnChangeLanguage.js
+++ src/components/functionalButtons/BtnChangeLanguage.js
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import Spanish from '../../images/spanishFlag.png';
 import English from '../../images/englishFlag.png';
-import { useState } from 'react';
 /* Queda pasar todo lo que requiere para su funcionamiento */
 function BtnChangeLanguage({language, setLanguage}) {
   return (
